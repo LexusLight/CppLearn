849919afb32172c68adb2209c0d6458422ad5663
Hideo Kojima <kostyaelizarov@mail.ru>
2019-12-25 23:22:44 +0300

    Life

--- Huffman/Huffman/Huffman/Huffman.cpp
+++ Huffman/Huffman/Huffman/Huffman.cpp
@@ -3,51 +3,199 @@
 #include <vector>
 #include <list>
 #include <map>
+#include <string> 
+#include <fstream>
 
 using namespace std;
+//Программа сначала зашифровывает картинку, потом берёт зашифрованный файл и расшифровывает.
+// Елизаров, ПР-20
 
-
-
-class Uzel
+class Node
 {
 	public:
 		int a;
-	Uzel *left, *right;
+		char c;
+	Node *left, *right;
+
+	Node() {
+		left = right = NULL;
+	}
+
+	Node(Node *L, Node *R) {
+		left = L;
+		right = R;
+		a = L->a + R->a;
+	}
 };
 
-int main()
+struct  MyCompare
 {
-	string s = "it is my striiiiing!!!!";
+	bool operator()(const Node* l, const Node* r)const {
+		return l->a < r->a;
+	}
+};
 
-	
-	/*
-		list<int> L;
-	list<int>::iterator itr1;
-	L.push_front(34);
-	L.push_back(134);
-	L.pop_back();
-	L.pop_front();
-	for (itr1 = L.begin; itr1 != L.end(); itr1++) {
-		cout << *itr1 << endl;
-
-	vector<int> x(10);
-	vector<int>::iterator itr;
-	x.push_back(34);
-	x.push_back(134);
-	x.push_back(3142);
-	x.push_back(124);
-	for (itr = x.begin; itr != x.end(); itr++) {
-		cout << *itr << endl;
-
-	Class AAA(){
-		AAA(int num, char ch){
-		a = num;
-		c = ch;
+/*void print(Node* root, unsigned k = 0) { //рекурсиввно строит дерево
+	if (root != NULL) {
+		print(root->left, k + 3);
+		for (unsigned i = 0; i < k; i++) {
+			cout << "  ";
 		}
 
-		bool operaror<(AAA t) const{
-		return a < t.a; // помогает делать sort по а
+		if (root->c) cout << root->a << " (" << root->c << ")" << endl;
+		else cout << root->a << endl;
+		print(root->right, k + 3);
+	}
+
+}*/
+
+vector<bool> code;
+map<char, vector<bool>> table;
+
+void BuildTable(Node *root) { //рекурсиввно вставляет в мап построенный по узлу код
+	
+	if (root->left != NULL) {
+		code.push_back(0);
+		BuildTable(root->left);
+	}
+	if (root->right != NULL) {
+		code.push_back(1);
+		BuildTable(root->right);
+	} 
+	if (root->c) {
+		table[root->c] = code;
+	}
+	if (code.size() > 0)
+		code.pop_back();
+}
+
+int main() 
+{
+	ifstream f("../../Step1.txt");
+	
+	map<char, int> m;//Обьявляем мап
+	map<char, int>::iterator i;
+
+	while (!f.eof()) { //считываем строку в мап
+		char c;
+		c= f.get();
+		m[c]++;
+	}
+
+	list<Node*> t;//Создаём класс и лист указателей на этот класс
+	for (map<char, int>::iterator itr = m.begin(); itr != m.end(); ++itr) {
+		Node *p = new Node;
+		p->c = itr->first;
+		p->a = itr->second;
+		t.push_back(p);
+	}
+
+	while (t.size() != 1) { //Посредством структкры делаем из первых двух элементов указатели на потомков и вычёркиваем их из листа, парента вставляем.
+		t.sort(MyCompare());
+
+		Node *SonL = t.front();
+		t.pop_front();
+		Node *SonR = t.front();
+		t.pop_front();
+
+		Node *parent = new Node(SonL, SonR);
+		t.push_back(parent);
+	}
+	
+	Node *root = t.front();
+
+	//print(root);
+	BuildTable(root);
+
+
+	f.clear();
+	f.seekg(0);
+	while(!f.eof()) {
+		char c;
+		c = f.get();
+		vector<bool> x = table[c];
+
+		for (int n = 0; n < x.size(); n++)
+			cout << x[n];
+	}
+	ofstream g("../../step2.lxs"); //Пишем шифр в файл
+
+	int count = 0; char buf = 0;
+
+	f.clear();
+	f.seekg(0);
+	while (!f.eof()) { 
+		char c;
+		c = f.get();
+		vector<bool> x = table[c];
+
+		for (int n = 0; n < x.size(); n++) {
+			buf = buf | x[n]<<(7 - count);
+			count++;
+			if (count == 8) {
+				count = 0;
+				g << buf; buf = 0;
+			}
 		}
 	}
-	*/
+	/*for (int i = 0; i < s.length(); i++) {
+		char c = s[i];
+		vector<bool> x = table[c];
+
+		for (int n = 0; n < x.size(); n++)
+			cout << x[n];
+	}*/
+	g.close();
+	f.close();
+	//Начинаем расшифровывать
+
+	ifstream F("../../Step2.lxs");
+	ofstream T("../../Step3.txt");
+
+	Node *p = root;
+	count = 0; char byte;
+	F >> byte;
+	vector<bool> vec;
+	while (!F.eof()) {
+		bool b = byte & (1 << (7-count));
+		if (b) p = p->right; else p = p->left;
+		if (p->left == NULL && p->right == NULL) { T << p->c; p = root; }
+		count++;
+		if (count == 8) { count = 0; F >> byte;}
+	}
+
+
+	T.close();
+
+	return 0;
 }
+/*
+	list<int> L;
+list<int>::iterator itr1;
+L.push_front(34);
+L.push_back(134);
+L.pop_back();
+L.pop_front();
+for (itr1 = L.begin; itr1 != L.end(); itr1++) {
+	cout << *itr1 << endl;
+
+vector<int> x(10);
+vector<int>::iterator itr;
+x.push_back(34);
+x.push_back(134);
+x.push_back(3142);
+x.push_back(124);
+for (itr = x.begin; itr != x.end(); itr++) {
+	cout << *itr << endl;
+
+Class AAA(){
+	AAA(int num, char ch){
+	a = num;
+	c = ch;
+	}
+
+	bool operaror<(AAA t) const{
+	return a < t.a; // помогает делать sort по а
+	}
+}
+*/
---
+++ Life/Life/Life.cpp
@@ -0,0 +1,201 @@
+﻿// Life.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+class Field{
+	string map[22][22];
+public:
+	Field (vector<int> VecX, vector<int> VecY , int C) {
+		for (int i = 0; i < C; i++) {
+			int x1 = VecX.back();
+			int y1 = VecY.back();
+			VecX.pop_back();
+			VecY.pop_back();
+			map[y1][x1] = "@ ";
+		}
+		PrintMap();
+	}
+
+	void PrintMap() {
+		for (int i = 1; i < 21; i++) {
+			for (int j = 1; j < 21; j++) {
+				if (map[i][j] == "@ ") {
+					cout << map[i][j];
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+
+	}
+
+	void CycleMap() {
+		string cycle_map[22][22];
+		int sum = 0; //количество живых клеток
+		for (int i = 1; i < 21; i++) {
+			sum = 0;
+			for (int j = 1; j < 21; j++) {
+				sum = 0;
+				sum += (map[i - 1]	[j ] == "@ ") ? 1 : 0;
+				sum += (map[i - 1]	[j + 1] == "@ ") ? 1 : 0;
+				sum += (map[i]		[j + 1] == "@ ") ? 1 : 0;
+				sum += (map[i + 1]	[j+1] == "@ ") ? 1 : 0;
+				sum += (map[i + 1]	[j] == "@ ") ? 1 : 0;
+				sum += (map[i+1]	[j - 1] == "@ ") ? 1 : 0;
+				sum += (map[i]		[j - 1] == "@ ") ? 1 : 0;
+				sum += (map[i - 1]	[j - 1] == "@ ") ? 1 : 0;
+
+				if (map[i][j] != "@ ") {//мёртвая клетка
+					if (sum == 3) {
+						cycle_map[i][j] = "@ ";
+					}
+					else {
+						cycle_map[i][j] = "  ";
+					}
+				}
+				else {//живая клетка
+					if (sum == 3 || sum == 2) {
+						cycle_map[i][j] = "@ ";
+					}
+					if (sum < 2 || sum > 3) {
+						cycle_map[i][j] = "  ";
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i < 21; i++) {
+			sum = 0;
+			for (int j = 1; j < 21; j++) {
+				map[i][j] = cycle_map[i][j];
+			}
+		}
+
+			
+
+		Sleep(1000);
+		system("cls");
+		PrintMap();
+
+		CycleMap();
+	}
+
+	~Field() {
+
+	}
+};
+
+int main()
+{	
+	SetConsoleCP(1251); //В свойствах консоли должен стоять шрифт Lucida
+	SetConsoleOutputCP(1251);
+	int count;
+	int y;
+	int x;
+	int aut;
+	vector<int> VX;
+	vector<int> VY;
+
+	cout << "Выберите действие:" << endl << "1. Автоматическая задача." << endl << "2. Задать самому." << endl;
+	cin >> aut;
+	system("cls");
+	if(aut == 2){
+		for (int i = 0; i < 20; i++) {	//Показываем структуру поля
+				for (int j = 0; j < 20; j++) {
+					cout << "* ";
+				}
+				cout << endl;
+		}
+		cout << "Поле 20 на 20." << endl;
+		cout << "Введите число изначительных клеток:" << endl;
+		cin >> count;
+		system("cls");
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				cout << "* ";
+			}
+			cout << endl;
+		}
+		for (int i = 1; i < count + 1; i++) {	//Вносим координаты клеток и выводим их расположение
+		cout << "Введите X " << i << " клетки:" << endl;
+		cin >> x;
+		VX.push_back(x);
+
+		cout << "Введите Y " << i << " клетки:" << endl;
+		cin >> y;
+		VY.push_back(y);
+		system("cls");
+
+		Field *LF = new Field(VX,VY,i);
+		LF->~Field();
+		}
+	}
+	else if (aut == 1) {
+		VX.push_back(3);// Это глайдер
+		VX.push_back(1);
+		VX.push_back(3);
+		VX.push_back(2);
+		VX.push_back(3);
+
+		VY.push_back(1);
+		VY.push_back(2);
+		VY.push_back(2);
+		VY.push_back(3);
+		VY.push_back(3);
+
+		VX.push_back(8);// Это глайдер этот глайдер и первый делают крутую систему
+		VX.push_back(6);
+		VX.push_back(8);
+		VX.push_back(7);
+		VX.push_back(8);
+
+		VY.push_back(2);
+		VY.push_back(3);
+		VY.push_back(4);
+		VY.push_back(3);
+		VY.push_back(4);
+
+		VX.push_back(16);// Это Осцилирация
+		VX.push_back(16);
+		VX.push_back(17);
+		VX.push_back(15);
+
+		VY.push_back(13);
+		VY.push_back(14);
+		VY.push_back(14);
+		VY.push_back(14);
+
+		VX.push_back(13);// Это глайдер
+		VX.push_back(11);
+		VX.push_back(13);
+		VX.push_back(12);
+		VX.push_back(13);
+
+		VY.push_back(1);
+		VY.push_back(2);
+		VY.push_back(2);
+		VY.push_back(3);
+		VY.push_back(3);
+		system("cls");
+
+		count = 19;
+	}
+	
+
+	system("cls");
+	Field *MP = new Field(VX, VY, count);
+	MP->CycleMap();
+}
+
